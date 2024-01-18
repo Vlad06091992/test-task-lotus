b84dba914f4e9349c40cf49a8e4dfc2c7d63c2c6
Vlad <Smirnov.ru92@mail.ru>
2024-01-18 12:10:16 +0500

    feat: base functional

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,87 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {ChangeEvent} from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+type State = {
+    searchString:string,
+    searchResults: Person[],
 }
 
-export default App;
+
+type Person = {
+    birth_year: string;
+    created: string;
+    edited: string;
+    eye_color: string;
+    films: string[];
+    gender: string;
+    hair_color: string;
+    height: string;
+    homeworld: string;
+    mass: string;
+    name: string;
+    skin_color: string;
+    species: string[];
+    starships: string[];
+    url: string;
+    vehicles: string[];
+};
+
+class App extends React.Component {
+  debouncedSearch: (...args:any) => void;
+  state:State
+  constructor(props:{}) {
+    super(props);
+    this.state = {
+      searchString: '',
+      searchResults: [],
+    };
+
+    this.debouncedSearch = this.debounce(this.search, 1000);
+  }
+
+  debounce(func:Function, delay:number) {
+    let timer:NodeJS.Timeout;
+    return (...args: any) => {
+      clearTimeout(timer);
+      timer = setTimeout(() => func.apply(this, args), delay);
+    };
+  }
+
+  handleInputChange = (event:ChangeEvent<HTMLInputElement>) => {
+      // debugger
+    const searchString = event.target.value;
+    this.setState({ searchString });
+    this.debouncedSearch(searchString);
+  };
+
+  search = async (string:string) => {
+    try {
+      const response = await axios.get(`https://swapi.dev/api/people/?search=${string}`);
+      this.setState({ searchResults: response.data.results });
+    } catch (error) {
+      console.error('Ошибка при выполнении запроса:', error);
+    }
+  };
+
+  render() {
+    const { searchString, searchResults } = this.state;
+
+    return (
+        <div>
+          <input
+              type="text"
+              value={searchString}
+              onChange={this.handleInputChange}
+              placeholder="Введите имя персонажа"
+          />
+          <ul>
+            {searchResults.map((result:Person) => (
+                <li key={result.created}>{result.name}</li>
+            ))}
+          </ul>
+        </div>
+    );
+  }
+}
+
+export default App
